bed6a38cd1929db4d55ef0213404986c6d33c26e
bellyrose831 <145377757+bellyrose831@users.noreply.github.com>
2025-05-03 15:46:49 -0500

    Update and rename Clean Data.sql to Clean & Expand Data.sql

--- Week 2: Pizza Runner/Clean & Expand Data.sql
+++ Week 2: Pizza Runner/Clean & Expand Data.sql
@@ -54,3 +54,22 @@ select order_id, runner_id,
 from runner_orders;
 
 select * from cleaned_runner_orders;
+
+-- expand date
+-- prompt: If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT 
+-- statement to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
+
+-- this doesn't impact the customer orders
+-- this doesn't impact pizza toppings
+-- this doesn't impact runner orders
+-- this doesn't impact runners
+-- Impacted: pizza names and pizza recipes tables
+
+-- pizza names:
+insert into pizza_names 
+values (3, "Supreme");
+
+-- pizza recipes 
+-- supreme will have: cheese (4), tomato sauce (12), onion (7), peppers (9), pepperoni (8), bacon (1)
+insert into pizza_recipes
+values (3, '1, 4, 7, 8, 9, 12');
